285932a74a2f447b526c9a8bbe73c28f5c46104c
nitudeka <nitumonideka327@gmail.com>
2018-10-07 15:25:34 +0530

    added post and get handler

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const helpers = require('./lib/helpers');
+const handlers = require('./lib/handlers');
+const config = require('./lib/config');
 
 const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url);
+  const parsedUrl = url.parse(req.url, true);
 
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+  const queryStringObject = parsedUrl.query;
   const headers = req.headers;
   const method = req.method.toLowerCase();
 
+  const decoder = new StringDecoder('utf8');
   let stream = '';
   req.on('data', (chunk) => {
-    stream += chunk;
+    stream += decoder.write(chunk);
   });
   req.on('end', () => {
-    console.log(method);
-    res.end('heyy!!!\n');
+    stream += decoder.end();
+    const dataObject = helpers.parseJsonToObject(stream);
+
+    const data = {
+      headers,
+      method,
+      queryStringObject,
+      path: trimmedPath,
+      payload: dataObject
+    }
+    
+    const choosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    choosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      const payloadString = JSON.stringify(payload);
+
+      res.setHeader('content-type', 'application/json')
+      res.writeHead(statusCode);
+      res.end(payloadString);
+    })
   })
 })
 
-server.listen(3001, () => {
-  console.log('server is listening on port 3001');
+const router = {
+  users: handlers.users
+}
+
+server.listen(config.port, () => {
+  console.log(`server is listening on port ${config.port}`);
 })
