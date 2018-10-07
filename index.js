2efcb096d58ddf8c8964439249e52bf698acdfe3
nitudeka <nitumonideka327@gmail.com>
2018-10-07 17:08:26 +0530

    added some comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Node dependencies
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
@@ -5,6 +6,7 @@ const helpers = require('./lib/helpers');
 const handlers = require('./lib/handlers');
 const config = require('./lib/config');
 
+// Initialize the http server
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
 
@@ -14,6 +16,7 @@ const server = http.createServer((req, res) => {
   const headers = req.headers;
   const method = req.method.toLowerCase();
 
+  // Decode the payload to utf8
   const decoder = new StringDecoder('utf8');
   let stream = '';
   req.on('data', (chunk) => {
@@ -21,8 +24,10 @@ const server = http.createServer((req, res) => {
   });
   req.on('end', () => {
     stream += decoder.end();
+    // Convert JSON to javascript object
     const dataObject = helpers.parseJsonToObject(stream);
 
+    // Data that have to sent to the handlers
     const data = {
       headers,
       method,
@@ -31,11 +36,13 @@ const server = http.createServer((req, res) => {
       payload: dataObject
     }
     
+    // Chose the handler the request should go to, route to not-found if nothing matches
     const choosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
     choosenHandler(data, (statusCode, payload) => {
       statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
       payload = typeof(payload) == 'object' ? payload : {};
 
+      // Conver the payload to JSON that have to send to the client
       const payloadString = JSON.stringify(payload);
 
       res.setHeader('content-type', 'application/json')
@@ -45,11 +52,13 @@ const server = http.createServer((req, res) => {
   })
 })
 
+// Acceptable routes
 const router = {
   register: handlers.register,
   login: handlers.login
 }
 
+// Start the server
 server.listen(config.port, () => {
   console.log(`server is listening on port ${config.port}`);
 })
